183cf0b07d8d9c0102d07f80e36854e443f85444
Angga Satya Putra <anggasp@hotmail.com>
2022-12-13 10:38:10 +0700

    [camera] Provide default exposure point if its null
    Some devices, particularly veux and peux misbehave if
    [CONTROL_AE_REGIONS] are set with null value.
    
    We should instead return the default value of the exposure point.
    
    Signed-off-by: Angga Satya Putra <anggasp@hotmail.com>

--- packages/camera/camera/android/src/main/java/io/flutter/plugins/camera/features/exposurepoint/ExposurePointFeature.java
+++ packages/camera/camera/android/src/main/java/io/flutter/plugins/camera/features/exposurepoint/ExposurePointFeature.java
@@ -14,12 +14,14 @@ import io.flutter.plugins.camera.CameraRegionUtils;
 import io.flutter.plugins.camera.features.CameraFeature;
 import io.flutter.plugins.camera.features.Point;
 import io.flutter.plugins.camera.features.sensororientation.SensorOrientationFeature;
+import java.util.Arrays;
 
 /** Exposure point controls where in the frame exposure metering will come from. */
 public class ExposurePointFeature extends CameraFeature<Point> {
 
   private Size cameraBoundaries;
   private Point exposurePoint;
+  private MeteringRectangle[] defaultExposureRectangle;
   private MeteringRectangle exposureRectangle;
   private final SensorOrientationFeature sensorOrientationFeature;
 
@@ -72,9 +74,14 @@ public class ExposurePointFeature extends CameraFeature<Point> {
     if (!checkIsSupported()) {
       return;
     }
-    requestBuilder.set(
-        CaptureRequest.CONTROL_AE_REGIONS,
-        exposureRectangle == null ? null : new MeteringRectangle[] {exposureRectangle});
+    if (defaultExposureRectangle == null) {
+       defaultExposureRectangle = requestBuilder.get(CaptureRequest.CONTROL_AE_REGIONS);
+     }
+     if (exposureRectangle != null) {
+       requestBuilder.set(CaptureRequest.CONTROL_AE_REGIONS, new MeteringRectangle[] {exposureRectangle});
+     } else if (shouldReset(requestBuilder)) {
+       requestBuilder.set(CaptureRequest.CONTROL_AE_REGIONS, defaultExposureRectangle);
+     }
   }
 
   private void buildExposureRectangle() {
@@ -96,4 +103,9 @@ public class ExposurePointFeature extends CameraFeature<Point> {
               this.cameraBoundaries, this.exposurePoint.x, this.exposurePoint.y, orientation);
     }
   }
+
+  private boolean shouldReset(CaptureRequest.Builder requestBuilder) {
+     MeteringRectangle[] currentRectangles = requestBuilder.get(CaptureRequest.CONTROL_AE_REGIONS);
+     return !Arrays.equals(currentRectangles, defaultExposureRectangle);
+   }
 }
